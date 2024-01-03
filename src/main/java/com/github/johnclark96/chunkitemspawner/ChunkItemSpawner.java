687a0cf504cf9e0e42c14b419bf23e7fb5e899fc
JClark <mlitvinkox96@gmail.com>
2024-01-03 18:01:16 -0500

    initial commit

---
+++ src/main/java/com/github/johnclark96/chunkitemspawner/ChunkItemSpawner.java
@@ -0,0 +1,17 @@
+package com.github.johnclark96.chunkitemspawner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChunkItemSpawner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

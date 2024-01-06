5a66e02a4be7531269abc01e3df628d246691aa9
JClark <mlitvinkox96@gmail.com>
2024-01-06 17:24:47 -0500

    added the initializekeys and updated chunkLoad register event

--- src/main/java/com/github/johnclark96/chunkitemspawner/ChunkItemSpawner.java
+++ src/main/java/com/github/johnclark96/chunkitemspawner/ChunkItemSpawner.java
@@ -1,14 +1,20 @@
 package com.github.johnclark96.chunkitemspawner;
 
+import com.github.johnclark96.chunkitemspawner.items.Item;
 import com.github.johnclark96.chunkitemspawner.listeners.ChunkLoad;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ChunkItemSpawner extends JavaPlugin {
 
+    private static ChunkItemSpawner plugin;
+
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new ChunkLoad(), this);
+        plugin = this;
+        this.saveDefaultConfig();
+        Item.initializeKeys(this);
+        getServer().getPluginManager().registerEvents(new ChunkLoad(this), this);
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "PLUGIN LOADED");
 
         // Plugin startup logic
@@ -18,5 +24,9 @@ public final class ChunkItemSpawner extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "PLUGIN UNLOADED");
+    }
+
+    public static ChunkItemSpawner getPlugin() {
+        return plugin;
     }
 }
